4dfe8ba1784b24a7a7dffbd6b6bda63f7a27e86d
Zero2404 <66071290+Zero2404@users.noreply.github.com>
2020-06-01 16:34:55 +0900

    Lesson1

--- HomeW3.java
+++ HomeW3.java
@@ -1,4 +1,12 @@
 package lesson1;
+       /* byte b = 25;
+        short s = 1111;
+        int i = 1234567;
+        char c = 'H';
+        long l = 1000L;
+        float f = 200.0f;
+        double d = 1.000001;
+        boolean bool = false;*/
 
 public class HomeW3 {
     public static void main(String[] args) {
